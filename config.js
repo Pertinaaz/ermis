94448064c11b6f855eacf6c5e63a2e0143de77d6
Pertinax <122640381+Pertinaaz@users.noreply.github.com>
2023-02-12 19:16:09 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     Client: {
-        TOKEN: "MTA3Mjk2MzE2MTE0NzMxNDI0Ng.GivO3R.lEhNRZpM6UOrU-2LmDhSCYlv1bhD9sC4X-LOsg",
-        APIkey: "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"
+        TOKEN: "",
+        APIkey: "
     },
 
     Users: {
-        BLACKLIST: ["903336815459713044"]
+        BLACKLIST: [""]
     }
 }
